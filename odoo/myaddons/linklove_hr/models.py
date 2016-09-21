57005bed388e8f1e710cbff84b51b060a96a75c8
cherry_linkloving <461807588@qq.com>
2016-09-21 11:20:59 +0800

    sale

--- odoo/myaddons/linklove_hr/models.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-# class linklove_hr(models.Model):
-#     _name = 'linklove_hr.linklove_hr'
-#     name = fields.Char()
--- odoo/myaddons/partner_internal_code/res_partner.py
+++ odoo/myaddons/partner_internal_code/res_partner.py
@@ -29,12 +29,14 @@ class partner(models.Model):
             name=name, args=args, operator=operator, limit=limit)
         return res
 
-    @api.model
-    def create(self, vals):
-        if not vals.get('internal_code', False):
-            vals['internal_code'] = self.env[
-                'ir.sequence'].get('partner.internal.code') or '/'
-        return super(partner, self).create(vals)
+    # @api.model
+    # def create(self, vals):
+    #     if not vals.get('internal_code', False):
+    #         vals['internal_code'] = self.env[
+    #             'ir.sequence'].get('partner.internal.code') or '/'
+    #     return super(partner, self).create(vals)
+
+
 
     _sql_constraints = {
         ('internal_code_uniq', 'unique(internal_code)',
