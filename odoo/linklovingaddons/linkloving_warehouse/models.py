e063bd5dcf9953a5d0f1b33ae1c29cfdd1b5e176
allen <iverson2937@163.com>
2016-09-26 16:49:31 +0800

    添加仓库模块

---
+++ odoo/linklovingaddons/linkloving_warehouse/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class linkloving_purchase(models.Model):
+#     _name = 'linkloving_purchase.linkloving_purchase'
+
+#     name = fields.Char()
