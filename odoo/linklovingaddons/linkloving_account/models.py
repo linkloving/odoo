3ecac0d1f1dbecbac4f9e9e8453c6ec9641fb3b0
Allen <iverson2937@163.com>
2016-09-30 10:25:26 +0800

    添加会计模块

---
+++ odoo/linklovingaddons/linkloving_account/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class linkloving_account(models.Model):
+#     _name = 'linkloving_account.linkloving_account'
+
+#     name = fields.Char()
