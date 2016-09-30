7b482c6f45712e374b6745c395548755dfd28678
Allen <iverson2937@163.com>
2016-09-30 18:32:16 +0800

    更新会计模块

--- odoo/linklovingaddons/linkloving_purchase/models/purchase_order.py
+++ odoo/linklovingaddons/linkloving_purchase/models/purchase_order.py
@@ -22,6 +22,19 @@ class PurchaseOrder(models.Model):
             'target': 'new',
         }
 
+    def action_picking_create(self, cr, uid, ids, context=None):
+        for order in self.browse(cr, uid, ids):
+            picking_vals = {
+                'picking_type_id': order.picking_type_id.id,
+                'partner_id': order.partner_id.id,
+                'date': order.date_order,
+                'origin': order.name,
+                'po_id': order.id
+            }
+            picking_id = self.pool.get('stock.picking').create(cr, uid, picking_vals, context=context)
+            self._create_stock_moves(cr, uid, order, order.order_line, picking_id, context=context)
+        return picking_id
+
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
