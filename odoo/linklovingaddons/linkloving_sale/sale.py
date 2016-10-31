3c1079589a279f71b2b9259ca0b734db9c44fb82
Allen <iverson2937@163.com>
2016-10-31 17:36:33 +0800

    添加销售数量

--- odoo/linklovingaddons/linkloving_sale/sale.py
+++ odoo/linklovingaddons/linkloving_sale/sale.py
@@ -17,6 +17,13 @@ class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
     tax_id = fields.Many2many('account.tax', required=True)
+    product_count = fields.Float(compute='get_product_count')
+
+    def get_product_count(self):
+        count = 0.0
+        for line in self.order_line:
+            count += line.product_uom_qty
+        self.product_count = count
 
 
 class SaleOrderLine(models.Model):
