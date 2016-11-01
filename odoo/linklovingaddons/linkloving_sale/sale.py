35783640f282c9472b897c7b8d345648b8b6da42
Allen <iverson2937@163.com>
2016-11-01 14:13:18 +0800

    序列号自增

--- odoo/linklovingaddons/linkloving_sale/sale.py
+++ odoo/linklovingaddons/linkloving_sale/sale.py
@@ -32,15 +32,15 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     tax_id = fields.Many2many(related='order_id.tax_id', store=True)
 
-    # def default_get(self, cr, uid, ids, context=None):
-    #     res = super(SaleOrder, self).default_get(cr, uid, ids, context=None)
-    #     if context:
-    #         context_keys = context.keys()
-    #         next_sequence = 1
-    #         if 'order_line' in context_keys:
-    #             if len(context.get('order_line')) > 0:
-    #                 next_sequence = len(context.get('order_line')) + 1
-    #     res.update({'sequence': next_sequence})
-    #     return res
+    def default_get(self, cr, uid, ids, context=None):
+        res = super(SaleOrderLine, self).default_get(cr, uid, ids, context=None)
+        if context:
+            context_keys = context.keys()
+            next_sequence = 1
+            if 'order_line' in context_keys:
+                if len(context.get('order_line')) > 0:
+                    next_sequence = len(context.get('order_line')) + 1
+        res.update({'sequence': next_sequence,'tax_id':context.get('default_tax_id')})
+        return res
 
 
